bd70fa46e6c9aff5d58d19364b6eb4439fb25d45
VVT <example@gmail.com>
2026-01-11 01:44:58 +0300

    add header (style, adaptation, js); mixin-ui-title

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,16 @@
 import '/src/sass/style.scss';
+
+
+const open = document.querySelector('.header__burger'),
+    close = document.querySelector('.header__menu-close'),
+    menu = document.querySelector('.header__menu');
+
+open.addEventListener('click', () => {
+    menu.classList.add('header__menu_active');
+    document.body.style.overflow = 'hidden';
+});
+
+close.addEventListener('click', () => {
+    menu.classList.remove('header__menu_active');
+    document.body.style.overflow = '';
+});
